26a6fab982e3f76aeede82d8cb120f33e40867b3
Jeremy Hawkins <coraxwolf@gmail.com>
2020-11-25 21:55:21 -0600

    Added Kelvin Temps to Converted Temps Output

--- main.c
+++ main.c
@@ -13,12 +13,12 @@
 
  int main()
  {
-     float celsius, fahrenheit, degrees; // celsius temp, fahrenheit temp, degrees input
+     float celsius, fahrenheit, kelvin, degrees; // celsius temp, fahrenheit temp, kelvin temp, degrees input
      char op = '\0'; // User Operation Character Input -- initialized to null
 
      void prompt()
      {
-         printf("Enter a temperature in degrees celsius or fahrenheit to have it converted into the other.\n");
+         printf("Enter a temperature in degrees celsius or fahrenheit to have it converted into the other and kelvin.\n");
          printf("Input _c_ to enter degrees celsius\n");
          printf("Input _f_ to enter degrees fahrenheit\n");
          printf("Input _x_ to exit program\n");
@@ -35,13 +35,15 @@
             printf("Enter Degrees Fahrenheit: ");
             scanf("%f", &degrees);
             celsius = (degrees - 32) * 5/9;
-            printf("%.2f Celsius = %.2f Fahrenheit\n", degrees, celsius);
+            kelvin = celsius + 273.15;
+            printf("%.2f Fahrenheit = %.2f Celsius or %.2f Kelvin\n", degrees, celsius, kelvin);
             break;
         case 'c' :
             printf("\nEnter Degrees Celsius: ");
             scanf("%f", &degrees);
             fahrenheit = (degrees * 9/5) + 32;
-            printf("%.2f Fahrenheit = %.2f Celsius\n", degrees, fahrenheit);
+            kelvin = degrees + 273.15;
+            printf("%.2f Celsius = %.2f Fahrenheit or %.2f Kelvin\n", degrees, fahrenheit, kelvin);
             break;
         case 'x' :
             printf("Exiting Program");
